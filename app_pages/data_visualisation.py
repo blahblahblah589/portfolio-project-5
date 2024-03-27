1a1339370cde145f77d59e65f29329b57c93820e
blahblahblah589 <hwright589@icloud.com>
2024-03-27 12:56:02 +0000

    continue readme.md development, continue development of all dashboard scripts

--- app_pages/data_visualisation.py
+++ app_pages/data_visualisation.py
@@ -29,20 +29,24 @@ def data_visualisation_body():
     if st.checkbox("Image Montage"): 
       st.write("* To refresh the montage, click on the 'Create Montage' button")
       my_data_dir = 'inputs/butterfly_moth/images'
-      labels = os.listdir(my_data_dir + '/validation')
+      labels = os.listdir('inputs/butterfly_moth/images/validation')
       label_to_display = st.selectbox(label="Select label", options=labels, index=0)
       if st.button("Create Montage"):      
-        create_image_montage(data=my_data_dir + '/validation',
+        create_image_montage(data=labels,
                       label_to_display=label_to_display,
                       nrows=3, ncols=3, figsize=(10,25))
       st.write("---")
+
+    if st.checkbox("Population Distribution"):
+        population_distribution(data='inputs/butterfly_moth/images/validation')
+        st.write("---")
 
 
 def create_image_montage(data, label_to_display, nrows, ncols, figsize):
     
     labels = os.listdir('inputs/butterfly_moth/images/validation')
     if label_to_display in labels:
-        montage = os.listdir(data + '/' + label_to_display)
+        montage = os.listdir('inputs/butterfly_moth/images/validation' + '/' + label_to_display)
         if nrows * ncols < len(montage):
             img_index = random.sample(montage, nrows * ncols)
         else:
@@ -55,9 +59,38 @@ def create_image_montage(data, label_to_display, nrows, ncols, figsize):
 
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
     for x_images in range(0, nrows * ncols):
-        img = imread(data + '/' + label_to_display + '/' + img_index[x_images])
+        img = imread('inputs/butterfly_moth/images/validation' + '/' + label_to_display + '/' + img_index[x_images])
         img_shape = img.shape
         fig.suptitle((f'{label_to_display.replace("_"," ").title()}'))
         axes[montage_plot[x_images][0], montage_plot[x_images][1]].imshow(img)
     plt.tight_layout()
+    plt.show()
+
+def population_distribution(data):
+
+    image_data = []
+    data = ('inputs/butterfly_moth/images/validation')
+    labels = os.listdir('inputs/butterfly_moth/images/validation')
+    for label in labels:
+        species = os.path.join(data, label)
+        count = len([name for name in os.listdir(species) if os.path.isfile(os.path.join(species, name))])
+
+        image_data.append({'Species': label, 'Recorded Population': count})
+
+    species_df = pd.DataFrame(image_data)
+    total_population = species_df['Recorded Population'].sum()
+
+    print(species_df)
+    print(f'Total Recorded Population: {total_population}')
+
+    population = species_df['Recorded Population']
+    species = species_df['Species']
+
+    plt.figure(figsize=(25,10))
+    plt.bar(species, population, color='green')
+    plt.ylim(ymin=60)
+    plt.xlabel('Species')
+    plt.ylabel('Population')
+    plt.title('Population Distribution')
+    plt.xticks(rotation=90)
     plt.show()
--- app_pages/ml_prediction_metrics.py
+++ app_pages/ml_prediction_metrics.py
@@ -3,3 +3,68 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.image import imread
 from src.machine_learning.evaluate import load_test_evaluation
+
+
+def ml_prediction_metrics_body():
+
+    st.write('## Train, Validation and Test Set: Labels Frequencies')
+
+    labels_distribution = plt.imread(f'outputs/v1/dataset_breakdown.png')
+    st.image(labels_distribution, caption='Dataset Breakdown')
+
+    st.write('## Model History')
+    st.write(
+        f'The model went through 5 versions before meeting the criteria '
+        f'expected for the business case.'
+    )
+    st.warning('### Version One:')
+    st.markdown('Conv2D layers (32, 64, 64, 128)')
+    st.markdown('Dropout rate of 30%')
+    st.markdown('Batch size of 20')
+    st.markdown('20 epochs')
+    st.markdown('Accuracy: 60%')
+
+    st.warning('### Version Two:')
+    st.markdown('Conv2D layers (32, 64, 64, 128, 256)')
+    st.markdown('Dropout rate of 30%')
+    st.markdown('Batch size of 40')
+    st.markdown('26 epochs')
+    st.markdown('Accuracy: 73%')
+
+    st.warning('### Version Three:')
+    st.markdown('Conv2D layers (64, 64, 128, 128, 256)')
+    st.markdown('Dropout rate of 30%')
+    st.markdown('Batch size of 35')
+    st.markdown('32 epochs')
+    st.markdown('Accuracy: Early stop prevented training moving past 4th Epoch.')
+
+    st.warning('### Version Four:')
+    st.markdown('Conv2D layers (32, 64, 128, 128, 256)')
+    st.markdown('Dropout rate of 40%')
+    st.markdown('Batch size of 50')
+    st.markdown('30 epochs')
+    st.markdown('Accuracy: 72%')
+
+    st.success('### Version Five:')
+    st.markdown('Conv2D layers (32, 64, 64, 128, 256)')
+    st.markdown('Dropout rate of 30%')
+    st.markdown('Batch size of 40')
+    st.markdown('45 epochs')
+    st.markdown('Accuracy: 76%')
+    st.markdown(
+        f'After fitting it proported a loss of 0.9708, and a val_loss of 0.8476. \n'
+        f'The validation loss being lower than the training loss suggest the model is \n'
+        f'generalising well to new data.\n After fitting, the model proported an \n'
+        f'accuracy level of 0.7140 and a val_loss of 0.7648. The validation accuracy being '
+        f'at 76% on a 100 class classification problem is acceptable. For reference, '
+        f'random allocation would result in a sucess rate of 1%.'
+    )
+
+    st.write('## Model Evaluation')
+
+    model_loss = plt.imread(f'outputs/v1/model_loss.png')
+    st.image(model_loss, caption='Model Version 5 Loss Graph')
+
+    model_loss = plt.imread(f'outputs/v1/model_accuracy.png')
+    st.image(model_loss, caption='Model Version 5 Accuracy Graph')
+    
